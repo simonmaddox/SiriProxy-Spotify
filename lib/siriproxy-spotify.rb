ba9aec054c83eb47387dfd11902f8d714bcb2f12
Simon Maddox <simon@simonmaddox.com>
2011-11-25 17:39:25 +0000

    Initial commit of project using new plugin style

---
+++ lib/siriproxy-spotify.rb
@@ -0,0 +1,35 @@
+require 'cora'
+require 'siri_objects'
+
+require 'open-uri'
+require 'json'
+require 'uri'
+
+#######
+# Control Spotify with your voice.
+# Simply say "Spotify, play me some Nirvana"
+######
+
+class SiriProxy::Plugin::Spotify < SiriProxy::Plugin
+  def initialize(config)
+    #if you have custom configuration options, process them here!
+  end
+
+  listen_for /(spotify|spotter five|spot of phi|spot fie|spot a fight|specify|spot if i|spotted by|stultify) play me some (.*)/i do |keyword, query|
+    
+    artist = URI.escape(query.strip)
+	  
+    results = JSON.parse(open("http://ws.spotify.com/search/1/track.json?q=#{artist}").read)
+    
+    if (results["tracks"].length > 1)
+      track = results["tracks"][0]
+
+      say "Playing #{track["name"]} by #{track["artists"][0]["name"]}"
+      `open #{track["href"]}`
+    else
+      say "I could not find anything by #{matchData[1]}"
+    end
+    
+    request_completed
+  end
+end
